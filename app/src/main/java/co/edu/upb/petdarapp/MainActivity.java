c1401009f3e77b0b4f55c8f0a2f9c261ba27b2e8
manuugit <manuela.hernandezr@upb.edu.co>
2022-04-12 17:49:53 -0500

    vista responsive solucionado

--- app/src/main/java/co/edu/upb/petdarapp/MainActivity.java
+++ app/src/main/java/co/edu/upb/petdarapp/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         btn_vac.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, VacunaNueva.class);
+                Intent i = new Intent(MainActivity.this, VistaVacuna.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/co/edu/upb/petdarapp/VistaVacuna.java
+++ app/src/main/java/co/edu/upb/petdarapp/VistaVacuna.java
@@ -3,12 +3,13 @@ package co.edu.upb.petdarapp;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Vacunas extends AppCompatActivity {
+public class VistaVacuna extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vacunas);
+
     }
 
 }
